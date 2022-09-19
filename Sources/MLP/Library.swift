218bc818fa34b8f18f93093aab22524df9a4a3d5
Bora Okumusoglu <bora@hetge.com>
2022-09-19 21:48:43 +1000

    NN structure with weights and bias

--- Sources/MLP/Library.swift
+++ Sources/MLP/Library.swift
@@ -1,26 +1,26 @@
 import Foundation
 
 public struct Value: CustomStringConvertible {
-    var data/*, grad*/: Double
+    var data, grad: Double
     var prev: [Value]
     var op: String
     var label: String
     
     init(_ data: Double, _ children:[Value] = [], _ op:String = "", label:String = "") {
         self.data = data
+        self.grad = 0.0
         self.prev = children
         self.op = op
         self.label = label
-        /*self.grad = grad*/
     }
 
     public func drawDot(_ level:Int = 0) -> String {
 
         let spacing = String(repeating: " |", count: level)
 
-        var drawResult = "\(spacing)_\(self.label) data \(self.data)|"
+        var drawResult = "\(spacing)_\(self.label)" + String(format:" data %.4f", self.data) + String(format:" grad %.4f", self.grad)
 
-        if self.prev.count > 1 {
+        if self.prev.count > 0 {
 
             var printOp = true
     
@@ -44,6 +44,12 @@ public struct Value: CustomStringConvertible {
     
     static func *(lhs: Value, rhs: Value) -> Value {
         return Value(lhs.data * rhs.data, [lhs, rhs],"*")
+    }
+
+    public func tanh() -> Value {
+        let x: Double = self.data
+        let t: Double = ( exp(2.0*x) - 1.0 ) / ( exp(2.0*x) + 1.0)
+        return Value(t , [self], "α") //α stands for tanh
     }
     
     public var description: String {
--- Sources/MLP/MLP.swift
+++ Sources/MLP/MLP.swift
@@ -1,10 +1,14 @@
 @main
 public struct MLP {
+
+    static var run01: Bool = false
     public static func main() {
         
-        let a = Value(2.0, label: "a")
-        let b = Value(-3.0, label: "b")
-        let c = Value(10.0, label: "c")
+        /*
+        //MARK: 00 Basic NN skeleton
+        var a = Value(2.0, label: "a")
+        var b = Value(-3.0, label: "b")
+        var c = Value(10.0, label: "c")
         var e = a*b
         e.label = "e"
         var d = e+c
@@ -13,9 +17,78 @@ public struct MLP {
         f.label = "f"
         var L = d*f
         L.label = "L"
-        //print(L)
-        //print(L.prev[0].prev)
-        //print(L.op)
         print(L.drawDot())
+        */
+
+
+        /*
+        //MARK: 01 Basic derivative
+        let h = 0.001
+
+        var a: Value
+        if run01 { a = Value(2.0, label: "a") } else  { a = Value(2.0, label: "a") }
+
+        var b = Value(-3.0, label: "b")
+        var c = Value(10.0, label: "c")
+        var e = a*b
+        e.label = "e"
+        var d = e+c
+        d.label = "d"
+        var f = Value(-2)
+        f.label = "f"
+        var L = d*f
+        L.label = "L"
+        let L1 = L.data
+        
+        print(L1)
+
+        if run01 { a = Value(2.0 + h, label: "a") }
+        b = Value(-3.0, label: "b")
+        c = Value(10.0, label: "c")
+        e = a*b
+        e.label = "e"
+        d = e+c
+        d.label = "d"
+        f = Value(-2)
+        f.label = "f"
+        L = d*f
+        L.label = "L"
+        var L2: Double
+        if run01 { L2 = L.data } else { L2 = L.data + h } 
+
+        print((L2-L1)/h)
+        */
+
+        
+        //MARK: 02 NN with Weights and Bias
+        //inputs x1, x2
+        let x1 = Value (2.0, label: "x1")
+        let x2 = Value (0.0, label: "x2")
+        
+        //weights w1, w2
+        let w1 = Value(-3.0, label: "w1")
+        let w2 = Value(1.0, label: "w2")
+
+        //bias of the neuron
+        let b = Value(6.8813735870195432, label: "bias")
+        
+        //x1*w1 + x2*w2 + b
+        var x1w1 = x1*w1
+        x1w1.label = "x1*w1"
+
+        var x2w2 = x2*w2
+        x2w2.label = "x2*w2"
+        
+        var x1w1x2w2 = x1w1 + x2w2
+        x1w1x2w2.label = "x1*w1 + x2*w2"
+        
+        var n = x1w1x2w2 + b
+        n.label = "n" 
+
+        var o = n.tanh()
+        o.label = "o"
+
+        print(o.drawDot())
+
     }
 }
