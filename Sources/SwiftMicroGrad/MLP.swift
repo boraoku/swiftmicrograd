c270e35206c5fac51cd81841847526147db0499b
Bora Okumusoglu <bora@hetge.com>
2022-09-23 16:04:47 +1000

    Parametric function to train to a MLP with inputs/outputs easily

--- Sources/SwiftMicroGrad/MLP.swift
+++ Sources/SwiftMicroGrad/MLP.swift
@@ -33,4 +33,65 @@ public class MLP {
         }
         return params
     }
+
+    public func train(inputs xs:[[Double]], outputs ys:[Double], 
+        loops: Int = 1000, stepForGradDescent: Double = 0.1, lossThreshold: Double = 0.0001, verbose: Bool = false) 
+    {
+        var finalLoss: Double = 1000.0
+        var finalLoopNo = loops
+
+        for i in 0...loops - 1 {
+            if verbose { print("\n---------------Loop no \(i+1)") }
+
+            //Forward Pass to estimate loss
+            let ydred = xs.map { self.feed($0) }
+            let losses = zip(ydred,ys).map() { ($0[0] - $1)**2 }
+            var loss = losses[0]
+            for i in 1..<losses.count {
+                loss = loss + losses[i]
+            }
+            if verbose { print("Loss Before Gradient Descent \(loss.data)") }
+
+            //Zerograd - important!
+            for p in self.parameters() {
+                p.grad = 0.0
+            }
+
+            //Backward Pass
+            loss.backward()
+
+            //Gradient Descent
+            for p in self.parameters() {
+                p.data += -stepForGradDescent * p.grad
+            }
+        
+            //Forward Pass to re-estimate loss
+            let ydred2 = xs.map { self.feed($0) }
+            let losses2 = zip(ydred2,ys).map() { ($0[0] - $1)**2 }
+            var loss2 = losses2[0]
+            for i in 1..<losses2.count {
+                loss2 = loss2 + losses2[i]
+            }
+            if verbose { print(" Loss After Gradient Descent \(loss2.data)") }
+            if verbose { print("----------------End loop no \(i+1)") }
+
+            //exit when lossThreshold is achieved
+            if loss2.data <= lossThreshold {
+                finalLoopNo = i+1
+                finalLoss = loss2.data
+                break
+            }
+
+            //record final loss
+            if (i+1) == loops {
+                finalLoss = loss2.data
+            }
+        }
+
+        print("\nFinal prediction after \(finalLoopNo) loops with loss \(finalLoss)")
+        let ydred = xs.map { self.feed($0) }
+        print("Target \(ys)")
+        print("Guess  \(ydred)")
+
+    }
 }
