c2ac69e62d9d1bbcacf3ae0821c52ec3c82d5351
Bora Okumusoglu <bora@hetge.com>
2022-09-27 22:58:14 +1000

    Adding midpoints generation showcasing the full prediction trend

--- Sources/SwiftMicroGrad/Main.swift
+++ Sources/SwiftMicroGrad/Main.swift
@@ -422,23 +422,48 @@ public struct SwiftMicroGrad {
         
         print("\nTraining...\n")
         let numberOfRuns = 5
+        let numberOfMids = xs.count-1
         var results = [Double](repeating: 0.0, count: numberOfRuns)
+        var midPoints:[Double] = []
+        for i in 0..<numberOfMids {
+            midPoints.append((xs[i][0] + xs[i+1][0]) / 2.0)
+        }
+        var midPointsResults = [Double](repeating: 0.0, count: numberOfRuns*numberOfMids)
         let queue = OperationQueue()
 
         for i in 0..<numberOfRuns {
-            queue.addOperation { 
+            queue.addOperation {
                 print("Running MLP \(i+1)/\(numberOfRuns)")
-                let n = MLP(1, [4,4,1]) 
-                n.train(inputs: xs, outputs: ys, loops:10000, stepForGradDescent: 0.05, lossThreshold: 10e-5, verbose: false) 
+                let n = MLP(1, [4,4,1])
+                n.train(inputs: xs, outputs: ys, loops:10000, stepForGradDescent: 0.05, lossThreshold: 10e-5, verbose: false)
                 results[i] = n.feed([2.0])[0].data
+                for j in 0..<numberOfMids {
+                    let midResult = n.feed([midPoints[j]])[0].data
+                    midPointsResults[i*numberOfMids + j] = midResult
+                }
             }
         }
 
         queue.waitUntilAllOperationsAreFinished()
 
         let average = results.reduce(0.0, +) / Double(numberOfRuns)
+        
+        var midPointsAverage:[Double] = []
+        for i in 0..<numberOfMids {
+            var midPointTotal: Double = 0.0
+            for j in 0..<numberOfRuns {
+                let midPointResult = midPointsResults[j*numberOfMids + i]
+                midPointTotal += midPointResult
+            }
+            let midAverage = midPointTotal/Double(numberOfRuns)
+            midPointsAverage.append(midAverage)
+        }
+        
         let resultsString = results.map { String(format:"%.4f", $0)  }.joined(separator: ", ")
         print("\nGuesses for Load levels at 2mm Creep Rate: " + resultsString)
         print("                               In Average: " + String(format:"%.4f", average))
+        for i in 0..<numberOfMids {
+            print("           Mid Point at " + String(format:"%.2f", midPoints[i]) + "mm Creep Rate: " + String(format:"%.4f", midPointsAverage[i]))
+        }
     }
 }
