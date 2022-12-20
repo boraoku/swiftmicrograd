4e4ed25b94387107e49850091d03f86282b90220
Bora Okumusoglu <bora@hetge.com>
2022-12-20 12:54:46 +1100

    Removed an unnecessary forward pass in training and improved verbose
    Verbose mode now prints Thread No / Loop No / Current Loss Value

--- Sources/SwiftMicroGrad/MLP.swift
+++ Sources/SwiftMicroGrad/MLP.swift
@@ -40,14 +40,13 @@ public class MLP {
     }
 
     public func train(inputs xs:[[Double]], outputs ys:[Double], 
-        loops: Int = 1000, stepForGradDescent: Double = 0.1, lossThreshold: Double = 0.0001, verbose: Bool = false) 
+                      loops: Int = 1000, stepForGradDescent: Double = 0.1, lossThreshold: Double = 0.0001, verbose: Bool = false, concurrencyCount: Int = -1)
     {
         var finalLoss: Double = 1000.0
         var finalLoopNo = loops
 
         for i in 0...loops - 1 {
-            if verbose { print("\n---------------Loop no \(i+1)") }
-
+            
             //Forward Pass to estimate loss
             let ydred = xs.map { self.feed($0) }
             let losses = zip(ydred,ys).map() { ($0[0] - $1)**2 }
@@ -55,7 +54,16 @@ public class MLP {
             for i in 1..<losses.count {
                 loss = loss + losses[i]
             }
-            if verbose { print("Loss Before Gradient Descent \(loss.data)") }
+            var concurrency = "---------------"
+            if concurrencyCount > 0 { concurrency = "--Thread no \(concurrencyCount)--" }
+            if verbose { print("\n\(concurrency) Loop no \(i+1) Loss \(loss.data)") }
+            
+            //exit when lossThreshold is achieved
+            if loss.data <= lossThreshold {
+                finalLoopNo = i+1
+                finalLoss = loss.data
+                break
+            }
 
             //Zerograd - important!
             for p in self.parameters() {
@@ -69,27 +77,10 @@ public class MLP {
             for p in self.parameters() {
                 p.data += -stepForGradDescent * p.grad
             }
-        
-            //Forward Pass to re-estimate loss
-            let ydred2 = xs.map { self.feed($0) }
-            let losses2 = zip(ydred2,ys).map() { ($0[0] - $1)**2 }
-            var loss2 = losses2[0]
-            for i in 1..<losses2.count {
-                loss2 = loss2 + losses2[i]
-            }
-            if verbose { print(" Loss After Gradient Descent \(loss2.data)") }
-            if verbose { print("----------------End loop no \(i+1)") }
-
-            //exit when lossThreshold is achieved
-            if loss2.data <= lossThreshold {
-                finalLoopNo = i+1
-                finalLoss = loss2.data
-                break
-            }
 
             //record final loss
             if (i+1) == loops {
-                finalLoss = loss2.data
+                finalLoss = loss.data
             }
         }
 
@@ -97,8 +88,8 @@ public class MLP {
 
         if verbose {
             print("\nFinal prediction after \(finalLoopNo) loops with loss \(finalLoss)")
-            print("Target \(ys)")
-            print("Guess  \(ydred)")
+            print("Training data outputs \(ys)")
+            print("Prediction outputs    \(ydred)")
         }
 
     }
